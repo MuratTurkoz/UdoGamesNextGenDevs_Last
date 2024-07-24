1e9d742ad53815c478bf0d134243e15dca84067c
Eyüp Öztürk <eyupoztrk04@gmail.com>
2024-07-24 22:34:25 +0300

    Player Turning

--- Assets/UdoGamesNextGen/Baran/Scripts/Swimming.cs
+++ Assets/UdoGamesNextGen/Baran/Scripts/Swimming.cs
@@ -1,13 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Swimming : MonoBehaviour
 {
-
     public float swimSpeed = 5f;
     public float amplitude = 0.25f;
     public float frequency = 1.0f;
@@ -15,27 +11,35 @@ public class Swimming : MonoBehaviour
     private Rigidbody myRigidBody;
     public Animator myAnimator;
     public FixedJoystick fixedJoystick;
-    
+    public float turnSpeed = 5f; // Yumuşak dönüş için bir hız değeri
+    public GameObject playerBody; // Oyuncunun gövdesini temsil eden GameObject
+
     // Start is called before the first frame update
     void Start()
     {
         this.transform.tag = "Player";
-       myRigidBody = GetComponent<Rigidbody>(); 
+        myRigidBody = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-       Swim();
+        Swim();
     }
-    private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.tag == "Water"){
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.tag == "Water") 
+        {
             isSwimming = true;
         }
     }
-    private void OnTriggerExit(Collider other) {
-        if(other.gameObject.tag == "Water"){
-           // isSwimming = false;
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if (other.gameObject.tag == "Water") 
+        {
+            isSwimming = false;
         }
     }
 
@@ -44,52 +48,59 @@ public class Swimming : MonoBehaviour
         isSwimming = false;
         myAnimator.SetBool("isDied", true);
     }
-   
-   private void Swim() {
-    if (isSwimming) {
-        myRigidBody.useGravity = false;    
 
-        float xInput = fixedJoystick.Horizontal;   
-        float yInput = fixedJoystick.Vertical;
+    private void Swim() 
+    {
+        if (isSwimming) 
+        {
+            myRigidBody.useGravity = false;    
 
-        UnityEngine.Vector3 targetVelocity = myRigidBody.velocity;
+            float xInput = fixedJoystick.Horizontal;   
+            float yInput = fixedJoystick.Vertical;
 
-        if (xInput == 0 && yInput == 0) {
+            Vector3 targetVelocity = myRigidBody.velocity;
 
-            if(myRigidBody.velocity.magnitude < 1){
-                myAnimator.SetBool("isMove", false);
+            if (xInput == 0 && yInput == 0) 
+            {
+                if (myRigidBody.velocity.magnitude < 1)
+                {
+                    myAnimator.SetBool("isMove", false);
+                }
+
+                // No player input, apply vertical resonance movement
+                float resonanceY = amplitude * Mathf.Sin(Time.time * frequency);
+                targetVelocity = new Vector3(0, resonanceY, 0);
+            } 
+            else 
+            {
+                // Player input detected, move accordingly
+                float xValue = xInput * swimSpeed;
+                float yValue = yInput * swimSpeed;
+                targetVelocity += new Vector3(xValue, yValue, 0);
+
+                myAnimator.SetBool("isMove", true);
+
+                // Determine the target rotation based on input direction
+                Quaternion targetRotation = Quaternion.identity;
+                if (xInput > 0) 
+                {
+                    targetRotation = Quaternion.Euler(0, 90, 0);
+                } 
+                else if (xInput < 0) 
+                {
+                    targetRotation = Quaternion.Euler(0, -90, 0);
+                }
+
+                // Smoothly transition to the target rotation for the player body
+                playerBody.transform.rotation = Quaternion.Lerp(playerBody.transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
             }
 
-            // No player input, apply vertical resonance movement
-            
-
-            float resonanceY = amplitude * Mathf.Sin(Time.time * frequency);
-            targetVelocity = new UnityEngine.Vector3(0, resonanceY, 0);
-        } else {
-            // Player input detected, move accordingly
-            float xValue = xInput * swimSpeed;
-            float yValue = yInput * swimSpeed;
-            targetVelocity += new UnityEngine.Vector3(xValue, yValue, 0);
-
-            myAnimator.SetBool("isMove", true);
-
-            if (xInput > 0) {
-                transform.localScale = new UnityEngine.Vector3(1, 1, 1);
-            } else if (xInput < 0) {
-                transform.localScale = new UnityEngine.Vector3(-1, 1, 1);
-            }
+            // Smoothly transition to the target velocity
+            myRigidBody.velocity = Vector3.Lerp(myRigidBody.velocity, targetVelocity, Time.deltaTime * 2.5f);
+        } 
+        else 
+        {
+            myRigidBody.useGravity = true; 
         }
-
-        // Smoothly transition to the target velocity
-        myRigidBody.velocity = UnityEngine.Vector3.Lerp(myRigidBody.velocity, targetVelocity, Time.deltaTime * 2.5f);
-    } else {
-        myRigidBody.useGravity = true; 
     }
 }
-
-
-
-}
-
-
-
