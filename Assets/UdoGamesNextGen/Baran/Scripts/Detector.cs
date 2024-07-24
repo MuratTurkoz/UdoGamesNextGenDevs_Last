ed2bb0460a454aa1cc978406411e9657808d6518
Baran Öner <baranoner5@gmail.com>
2024-07-24 17:13:03 +0300

    Enemy Movement Finished

---
+++ Assets/UdoGamesNextGen/Baran/Scripts/Detector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour
+{
+    public GameObject enemyObject;
+    private void OnTriggerEnter(Collider other) {
+       if(other.gameObject.tag == "Obstackle"){
+        if(enemyObject.GetComponent<EnemySwimming>().yAxis){
+        enemyObject.GetComponent<EnemySwimming>().direction *= -1;
+        enemyObject.transform.localScale = new Vector3(1,enemyObject.GetComponent<EnemySwimming>().direction, 1);
+        }
+        else if(enemyObject.GetComponent<EnemySwimming>().xAxis){
+        enemyObject.GetComponent<EnemySwimming>().direction *= -1;
+        enemyObject.transform.localScale = new Vector3(enemyObject.GetComponent<EnemySwimming>().direction,1, 1);
+        }
+       } 
+        
+       
+    }
+}
